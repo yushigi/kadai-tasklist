2b501cc8b08f063a72993ceec61ee2a80927d93c
yushigi.hong <yushigi.hong@rakuten.com>
2018-05-31 03:21:53 +0000

    CRUD tasks

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,7 +4,7 @@
 
 <h1>タスク新規作成ページ</h1>
 
-    {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
