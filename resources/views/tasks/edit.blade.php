b93ee42ece704d5aeb85591bb98f59c4796fa127
yushigi.hong <yushigi.hong@rakuten.com>
2018-06-04 06:51:57 +0000

    boostrap design tasklist

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,24 @@
 
 <h1>id: {{ $tasks->id }} のタスク編集ページ</h1>
 
+<div class="row">
+        <div class=<div class="col-xs-12" "col-sm-offset-2 col-sm-8" "col-md-offset-8 col-md-8" "col-lg-offset-3 col-lg-6">
+
     {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
-    
-         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        <div class="form-group">
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status', null, ['class'=>'form-control']) !!}
+        </div>
+        
+        <div class="form-group">
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content', null, ['class'=>'form-control']) !!}
+        </div>
 
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class'=>'btn btn-default']) !!}
 
     {!! Form::close() !!}
+        </div>
+</div>
 
 @endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,14 +4,27 @@
 
     <h1>タスクリスト</h1>
 
-        @if (count($tasks) > 0)
-            <ul>
+       @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>ステータス</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
                 @foreach ($tasks as $task)
-                   <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}  {{ $task->status }}</li>
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
                 @endforeach
-            </ul>
-        @endif
+            </tbody>
+        </table>
+    @endif
     
-    {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
+    {!! link_to_route('tasks.create', '新規タスクの投稿',null,['class' => 'btn btn-primary']) !!}
 
 @endsection
